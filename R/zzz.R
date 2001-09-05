4f98e3a2593f4f5d50a91eeaff1f966e499cfec9
Robert Gentleman <rgentlem@fhcrc.org>
2001-09-05 14:31:04 +0000

    fixed the loading of annotate
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/annotate@51 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,13 @@
-.First.lib <- function(lib, pkg) {
+.First.lib <- function(libname, pkgname, where) {
   require(methods)
+   if(missing(where)) {
+        where <- match(paste("package:", pkgname, sep=""), search())
+        if(is.na(where)) {
+            warning(paste("Not a package name: ",pkgname))
+            return()
+        }
+        where <- pos.to.env(where)
+    }
+   .initClasses(where)
+
 }
