1b6924f0b8d531e168c31398c7364d50872ea7fe
Robert Gentleman <rgentlem@fhcrc.org>
2001-10-31 01:11:51 +0000

    made some changes--hopefully the right ones
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/annotate@144 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,15 @@
 .First.lib <- function(libname, pkgname, where) {
   if( version$major == "1" && as.numeric(version$minor) < 4 )
     require(RSMethods)
-  else 
-    require(methods)
-   if(missing(where)) {
-        where <- match(paste("package:", pkgname, sep=""), search())
-        if(is.na(where)) {
-            warning(paste("Not a package name: ",pkgname))
-            return()
-        }
-        where <- pos.to.env(where)
-    }
-   .initClasses(where)
-
+##  else
+##    require(methods)
+##   if(missing(where)) {
+##        where <- match(paste("package:", pkgname, sep=""), search())
+##        if(is.na(where)) {
+##            warning(paste("Not a package name: ",pkgname))
+##            return()
+##        }
+##        where <- pos.to.env(where)
+##    }
+##   .initClasses(where)
 }
