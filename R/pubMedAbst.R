c157d5a81a8c3a277454c7cc743fdee1aada1b01
Jeff Gentry <jgentry@jimmy.harvard.edu>
2002-04-04 18:23:35 +0000

    First pass at the pubMedAbst class (still needs to flesh out a few slots)
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/annotate@808 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/pubMedAbst.R
@@ -0,0 +1,85 @@
+.initPubMedAbst <- function(where) {
+
+    ## Define the class structure of the pubMedAbst object
+    setGeneric("pubMedAbst", function(object)
+               standardGeneric("pubMedAbst"), where=where)
+
+    setClass("pubMedAbst",
+             representation(authors="vector", abstText="character",
+             articleTitle="character", journal="character",
+             pubDate="character"), where=where)
+
+    ## Define accessors
+    if (is.null(getGeneric("authors")))
+        setGeneric("authors", function(object)
+                   standardGeneric("authors"), where=where)
+
+    if (is.null(getGeneric("abstText")))
+        setGeneric("abstText", function(object)
+                   standardGeneric("abstText"), where=where)
+
+    if (is.null(getGeneric("articleTitle")))
+        setGeneric("articleTitle", function(object)
+                   standardGeneric("articleTitle"), where=where)
+
+    if (is.null(getGeneric("journal")))
+        setGeneric("journal", function(object)
+                   standardGeneric("journal"), where=where)
+
+    if (is.null(getGeneric("pubDate")))
+        setGeneric("pubDate", function(object)
+                   standardGeneric("pubData"), where=where)
+
+    ## Methods
+    setMethod("authors", "pubMedAbst", function(object)
+              object@authors, where=where)
+    setMethod("abstText", "pubMedAbst", function(object)
+              object@abstText, where=where)
+    setMethod("articleTitle", "pubMedAbst", function(object)
+              object@articleTitle, where=where)
+    setMethod("journal", "pubMedAbst", function(object)
+              object@journal, where=where)
+    setMethod("pubDate", "pubMedAbst", function(object)
+              object@pubData, where=where)
+}
+
+buildPubMedAbst <- function(xml) {
+    ## Passed in a XML tree detailing a single article
+    ## will parse the XML and create a new class
+
+    xmlArticle <- xml[[1]]["MedlineCitation"][[1]]["Article"]
+
+    articleTitle <- xmlArticle[[1]]["ArticleTitle"]
+    articleTitle <- as.character(xmlChildren(articleTitle[[1]])$text)[5]
+
+    abstText <- xmlArticle[[1]]["Abstract"][[1]]["AbstractText"]
+    abstText <- as.character(xmlChildren(abstText[[1]])$text)[5]
+
+    xmlJournal <- xmlArticle[[1]]["Journal"]
+
+    pubDateBase <- xmlJournal[[1]]["JournalIssue"][[1]]["PubDate"]
+    pubDateMonth <- pubDateBase[[1]]["Month"]
+    pubDateMonth <- as.character(xmlChildren(pubDateMonth[[1]])$text)[5]
+
+    pubDateYear <- pubDateBase[[1]]["Year"]
+    pubDateYear <- as.character(xmlChildren(pubDateYear[[1]])$text)[5]
+    pubDate <- paste(pubDateMonth,pubDateYear)
+
+    authorList <- xmlArticle[[1]]["AuthorList"]
+    authors <- vector()
+    for (i in 1:length(xmlChildren(authorList[[1]]))) {
+        curAuthor <- authorList[[1]][i]
+        last <-
+            as.character(xmlChildren(curAuthor[[1]]["LastName"][[1]])$text)[5]
+        first <-
+            as.character(xmlChildren(curAuthor[[1]]["ForeName"][[1]])$text)[5]
+        mid <-
+            as.character(xmlChildren(curAuthor[[1]]["Initials"][[1]])$text)[5]
+
+        authors[i] <- paste(first,mid,last)
+    }
+
+    newPMA <- new("pubMedAbst", articleTitle=articleTitle,
+                  abstText=abstText, pubDate=pubDate,authors=authors)
+    return(newPMA)
+}
