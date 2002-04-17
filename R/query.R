efd7823b5f77dfe148e7d0d8820afc7aadfcf978
Robert Gentleman <rgentlem@fhcrc.org>
2002-04-17 19:07:07 +0000

    added some parameters to ll.htmlpage, updated the hgu95A docs
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/annotate@1009 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/query.R
+++ R/query.R
@@ -283,7 +283,8 @@ genelocator <- function(x) {
   }
 }
 
-ll.htmlpage <- function (genelist, filename, title, othernames)
+ll.htmlpage <- function (genelist, filename, title, othernames,
+                         table.head, table.center=TRUE)
 {
     outfile <- file(filename, "w")
     cat("<html>", "<head>", "<TITLE>BioConductor Gene Listing</TITLE>",
@@ -293,8 +294,16 @@ ll.htmlpage <- function (genelist, filename, title, othernames)
     if( !missing(title) )
         cat("<CENTER><H1 ALIGN=\"CENTER\">", title, " </H1></CENTER>\n",
             file=outfile, sep = "\n")
+
+    if( table.center )
+        cat("<CENTER> \n", file=outfile)
+
     cat("<TABLE BORDER=4>", "<CAPTION> Locus Link Genes </CAPTION>",
         file = outfile, sep = "\n")
+    if( !missing(table.head) ) {
+        headout <- paste("<TH>", table.head, "</TH>")
+        cat("<TR>", headout, "</TR>", file=outfile, sep="\n")
+    }
     rh <- "<TD> <A HREF=\"http://www.ncbi.nlm.nih.gov/LocusLink/LocRpt.cgi?l="
     nrows <- length(genelist)
     rows <- paste(rh, genelist, "\">", genelist, "</A> </TD>",
@@ -312,6 +321,10 @@ ll.htmlpage <- function (genelist, filename, title, othernames)
     for (i in 1:nrows)
         cat("<TR>", rows[i], "</TR>", file = outfile, sep = "\n")
     cat("</TABLE>", "</body>", "</html>", sep = "\n", file = outfile)
+
+    if( table.center )
+        cat("</CENTER> \n", file=outfile)
+
     close(outfile)
 }
 
