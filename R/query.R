1012ad18313142107071c0e03eb2171e7d8e7ec3
Jeff Gentry <jgentry@jimmy.harvard.edu>
2002-11-08 18:15:52 +0000

    check for XML version
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/annotate@2272 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/query.R
+++ R/query.R
@@ -153,6 +153,15 @@ accessionToUID <- function(...,db=c("genbank","pubmed")) {
     ## indirection.
 
     require(XML) || stop("XML package is unavailable!")
+    ## Make sure that XML version is what we require
+    ## !!! Need to make this automatic, hardcode version in for now
+    xmlVers <- package.description("XML",fields="Version")
+    reqXmlVers <- "0.92-2"
+    if (compareVersion(xmlVers,reqXmlVers) < 0)
+        stop(paste("Installed XML version is ",xmlVers,
+                   " while this functionality requires ", reqXmlVers,
+                   ":  Please update your XML package.",sep=""))
+
     options(show.error.messages = FALSE)
     on.exit(options(show.error.messages = TRUE))
     retVal <- NULL
