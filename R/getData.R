efbb3792af96ecb427f9d56c62d4e6e065e85e05
Robert Gentleman <rgentlem@fhcrc.org>
2003-03-06 17:36:25 +0000

    added the getSYMBOL etc code
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/annotate@3530 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/getData.R
@@ -0,0 +1,48 @@
+##copyright 2002 R. Gentleman, all rights reserved
+##helper functions for dealing with data environments (soon to be hash
+##tables)
+
+getGO <- function(x, data="hgu95a") {
+     library(data, character.only=TRUE)
+     GOenv <- get(paste(data, "GO",sep=""))
+     multiget(x, env=GOenv)
+ }
+
+ getGOdesc <- function(x, which=c("MF","BP","CC") ) {
+     require(GO) || stop("need the GO library")
+     d <- match.arg(which)
+     de <- switch(d, MF=GOMFID2TERM, BP=GOBPID2TERM, CC=GOCCID2TERM,
+                  stop(paste(which, "did not match a GO data type")))
+     ans <- multiget(x, env=de)
+     ans[is.na(ans)] <- NULL
+     ans
+ }
+
+  getSYMBOL <- function(x, data="hgu95a") {
+     library(data, character.only=TRUE)
+     GOenv <- get(paste(data, "SYMBOL",sep=""))
+     unlist(multiget(x, env=GOenv))
+ }
+
+  getPMID <- function(x, data="hgu95a") {
+      library(data, character.only=TRUE)
+      PMenv <- get(paste(data, "PMID", sep=""))
+      multiget(x, env=PMenv)
+  }
+
+  getLL <- function(x, data="hgu95a") {
+      library(data, character.only=TRUE)
+      LLenv <- get(paste(data, "LOCUSID", sep=""))
+      unlist(multiget(x, env=LLenv))
+  }
+
+.initCont <- function(where) {
+  if( !isGeneric("contents") && !exists("contents", mode="function") )
+       setGeneric("contents", function(object)
+                  standardGeneric("contents"),
+               where=where)
+
+  setMethod("contents", "environment",
+     function(object) 
+         multiget(ls(env=env), env=env), where=where)
+}
