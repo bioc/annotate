6f8f23ebd6ab02d40b439c580b70361fd2f65456
jhnzhang <unknown>
2003-08-20 19:30:50 +0000

    Improvements.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/annotate@5376 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/findNeighbors.R
+++ R/findNeighbors.R
@@ -1,51 +1,144 @@
-findNeighbors <- function(chrLoc, llID,
-                         chromosome, upBase, downBase, agreessive = TRUE){
+findNeighbors <- function(chrLoc, llID, chromosome, upBase, downBase){
 
     require(chrLoc, character.only = TRUE) ||
-                           stop("Chromomosome location data unavailable!")
-    # Find the location for the target gene
-    location <- as.numeric(get(llID, get(paste(chrLoc,
+                           stop(paste("Chromomosome location chrLoc",
+                                      "is not available on the system",
+                                      "Either build one or get one from",
+                                      "BioConductor"))
+
+    if(checkArgs(llID, chromosome, upBase, downBase) == "swap"){
+        temp <- upBase
+        upBase <- downBase
+        downBase <- temp
+    }
+    upBase <- as.numeric(ifelse(missing(upBase), 0, upBase))
+    downBase <- as.numeric(ifelse(missing(downBase), 0, downBase))
+    if(missing(chromosome)){
+        chromosome <- findChr4LL(llID, get(paste(chrLoc,
+                                                 "LOCUSID2CHR", sep = "")),
+                                 get(paste(chrLoc,"ORGANISM", sep = "")))
+    }
+    if(!missing(llID)){
+        # Find the location for the target gene
+        location <- as.numeric(get(llID, get(paste(chrLoc,
                                       chromosome, "START", sep = ""))))
-    if(agreessive){
-        # agreessive search locat genes whose transcription either starts
-        # (upstream) or ends (downstream) within the defined region.
-
-        # Convert the environment to a matrix for searching
-        uLoc <- cbind(ls(get(paste(chrLoc, chromosome, "START", sep = ""))),
-                       unlist(multiget(ls(get(paste(chrLoc, chromosome,
-                                             "START", sep = ""))),
-                       get(paste(chrLoc, chromosome, "START", sep = "")))))
-        dLoc <- cbind(ls(get(paste(chrLoc, chromosome, "END", sep = ""))),
-                       unlist(multiget(ls(get(paste(chrLoc, chromosome,
-                                             "END", sep = ""))),
-                       get(paste(chrLoc, chromosome, "END", sep = "")))))
     }else{
-        # non-agreessive search locat genes whose transcription either
-        # ends (up stream) or start (down stream within the defined region.
-
-        # Convert the environment to a matrix for searching
-        uloc <- cbind(ls(get(paste(chrLoc, chromosome, "END", sep = ""))),
-                       unlist(multiget(ls(get(paste(chrLoc, chromosome,
-                                             "END", sep = ""))),
-                       get(paste(chrLoc, chromosome, "END", sep = "")))))
-
-        dloc <- cbind(ls(get(paste(chrLoc, chromosome, "START", sep = ""))),
-                       unlist(multiget(ls(get(paste(chrLoc, chromosome,
-                                             "START", sep = ""))),
-                       get(paste(chrLoc, chromosome, "START", sep = "")))))
+        location <- 0
+    }
+    lowerB <- getBoundary(location, downBase, TRUE)
+    upperB <- getBoundary(location, upBase, FALSE)
+    neighbors <- list()
+    # There may be chances that a llID be mapped to genes on different CHR
+    for(i in chromosome){
+        start <- unlist(multiget(ls(get(paste(chrLoc, chromosome,
+                                              "START", sep = ""))),
+                                 get(paste(chrLoc, chromosome,
+                                           "START", sep = ""))),
+                        use.names = TRUE)
+        end <- unlist(multiget(ls(get(paste(chrLoc, chromosome,
+                                              "END", sep = ""))),
+                                 get(paste(chrLoc, chromosome,
+                                           "END", sep = ""))),
+                        use.names = TRUE)
+        # greb the ones in the range
+        foundLLs <- c(start[start >= lowerB & start <= upperB],
+                      end[end >= lowerB & end <= upperB])
+        if(length(foundLLs) != 0){
+            temp <- unique(names(foundLLs))
+            foundLLs <- gsub("(^.*)\\..*", "\\1", temp)
+            names(foundLLs) <- gsub("^.*\\.(.*)", "\\1", temp)
+            # Remove LLs named Unconfident if one named Confident exists
+            if(any(duplicated(foundLLs))){
+                foundLLs <- c(foundLLs[names(foundLLs) == "Confident"],
+                              foundLLs[names(foundLLs) != "Confident"])
+                foundLLs <- foundLLs[!duplicated(foundLLs)]
+            }
+            neighbors[[as.character(i)]] <- foundLLs
+        }
     }
 
-    uNeighbor <- as.vector(uLoc[as.numeric(uLoc[,2]) <=
-                          (location + as.numeric(upBase)) &
-                           as.numeric(uLoc[,2]) > location, 1])
-    dNeighbor <- as.vector(dLoc[as.numeric(dLoc[,2]) >=
-                          (location - as.numeric(downBase)) &
-                           as.numeric(dLoc[,2]) < location, 1])
 
-    neighbors <- c(uNeighbor, dNeighbor)
     if(length(neighbors) == 0){
-        return(NULL)
+        warning("No Genes in the defined region satisfy the condition")
     }else{
         return(neighbors)
+    }
+}
+
+checkArgs <- function(llID, chromosome, upBase, downBase){
+    # llID is not required if search for genes within a range
+    if(missing(llID)){
+        # Both upBase, downBase, and chromosome must be there if
+        # llID is missing
+        if(any(missing(upBase), missing(downBase), missing(chromosome))){
+            stop(paste("Search can not be conducted with llID and",
+                       "at least one of upBase, downBase and ",
+                       "chromosome missing"))
+        }else{
+            if(as.numeric(upBase) < as.numeric(downBase)){
+                warning(paste("upBase value is smaller then downBase",
+                              "value. Values have been swapped"))
+                return("swap")
+            }
+            if(as.numeric(upBase) == as.numeric(downBase)){
+                stop("upBase and downBase can not be the same")
+            }
+        }
+    }else{
+        if(missing(upBase)){
+            warning(paste("upBase is missing. Search will be",
+                          "conducted for genes downstream only"))
+        }
+        if(missing(downBase)){
+            warning(paste("downBase is missing. Search will be",
+                          "conducted for genes upstream only"))
+        }
+    }
+    return("OK")
+}
+
+findChr4LL <- function(llID, chrEnv, organism){
+    options(show.error.message = FALSE)
+    chr <- try(get(llID, chrEnv))
+    options(show.error.message = TRUE)
+    if(inherits(chr, "try-error")){
+        stop(paste("LocusLink id", llID, "could not be found in any",
+                   "of the chromosomes in the data package"))
+    }else{
+        if(length(chr) == 1){
+            if(!is.element(chr, getValidChr(organism))){
+                stop(paste("LocusLink id", llID, "is currently",
+                           "not known to be associated with any",
+                           "chromosome"))
+            }
+            return(chr)
+        }else{
+            chr <- chr[is.element(chr, getValidChr(organism))]
+            return(unique(chr))
+        }
+    }
+}
+
+
+getValidChr <- function(organism){
+    switch(toupper(organism),
+           HUMAN = return(c(1:22, "X", "Y")),
+           stop(paste("Unknow organism", organism)))
+}
+
+getBoundary <- function(loc, base, lower = TRUE){
+    if(as.numeric(loc) == 0){
+        return(base)
+    }else{
+        if(lower){
+            boundary <- as.numeric(loc) - base
+            if(boundary < 0){
+                return(0)
+            }else{
+                return(boundary)
+            }
+        }else{
+            return(as.numeric(loc) + base)
+        }
     }
 }
