a8013f069159dfeb5af4e1737f4407f87fab6665
Jianhua Zhang <jzhang@jimmy.harvard.edu>
2004-07-15 13:47:00 +0000

    A function that counts ACCNUM types added.JZ
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/annotate@9413 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/ACCNUMStats.R
@@ -0,0 +1,30 @@
+ACCNUMStats <- function(pkgName){
+    if(!require(pkgName, character.only = TRUE)){
+        stop(paste("Data package", pkgName, "is not available"))
+    }
+    accs <- as.list(get(paste(pkgName, "ACCNUM", sep = "")),
+                    pos = match(paste("package:", pkgName, sep = ""),
+                    search()) )
+
+    return(table(unlist(sapply(accs, whatACC))))
+}
+
+whatACC <- function(accs){
+    accs <- strsplit(accs, ";")
+    if(regexpr("^[a-zA-Z]{2}\\.[0-9]+$", accs[1]) > 0){
+        return("UniGene")
+    }
+    if(regexpr("^(NP_)|(NG_)|(NM_)|(NC_)|(XR_)|(XM_)|(XP_)[0-9]+$",
+               accs[1]) > 0){
+        return("RefSeq")
+    }
+    if(regexpr("^[a-zA-Z]+[0-9]+$", accs[1]) > 0){
+        return("GBAcc")
+    }
+    if(regexpr("^[0-9]+$", accs[1]) > 0){
+        return("Image")
+    }
+
+    return(NA)
+}
+
