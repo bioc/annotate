4c3519882837b1652029618d8ff74426dc825763
James MacDonald <jmacdon@med.umich.edu>
2004-07-29 19:56:35 +0000

    JWM: Changes to useProbeInfo, added some necessary functions
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/annotate@9565 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getSeq4ACC.R
+++ R/getSeq4ACC.R
@@ -1,35 +1,50 @@
 getSeq4Acc <- function(accNum){
-    #options(show.error.messages = FALSE)
-    return(getSEQ(accNum))
-    #options(show.error.messages = TRUE)
+  #options(show.error.messages = FALSE)
+  return(getSEQ(accNum))
+  #options(show.error.messages = TRUE)
 }
 
 getGI <- function(accNum){
-    # Get the gi based on the Accession number
-    gi <- readLines(paste("http://www.ncbi.nlm.nih.gov/entrez/",
-                          "query.fcgi?db=Nucleotide&cmd=search&term=",
-                          accNum, sep = ""))
-    gi <- gsub(paste(".*gi\\|([0-9]+)\\|[a-zA-Z0-9]+\\|", accNum,
-                     ".*", sep = ""), "\\1",
-               gi[grep(paste("gi\\|.*\\|[a-zA-Z0-9]+\\|", accNum, ".*",
-                                    sep = ""), gi)])
-    if(length(gi) == 0){
-        stop(paste("Can't obtain a gi number for", accNum))
-    }else{
-        return(gi)
-    }
+  # Get the gi based on the Accession number
+  gi <- readLines(paste("http://www.ncbi.nlm.nih.gov/entrez/",
+                        "query.fcgi?db=Nucleotide&cmd=search&term=",
+                        accNum, sep = ""))
+  gi <- gsub(paste(".*gi\\|([0-9]+)\\|[a-zA-Z0-9]+\\|", accNum,
+                   ".*", sep = ""), "\\1",
+             gi[grep(paste("gi\\|.*\\|[a-zA-Z0-9]+\\|", accNum, ".*",
+                           sep = ""), gi)])
+  if(length(gi) == 0){
+    stop(paste("Can't obtain a gi number for", accNum))
+  }else{
+    return(gi)
+  }
 }
 
 getSEQ <- function(gi){
-
-    seq <- readLines(paste("http://www.ncbi.nlm.nih.gov/entrez/batchseq.cgi?",
-                 "cmd=&txt=on&save=&cfm=&list_uids=", gi, "&",
-                 "db=nucleotide&extrafeat=16&term=&view=fasta&",
-                 "dispmax=20&SendTo=t&__from=&__to=&__strand=", sep = ""))
-
-    if(length(seq) == 0){
-        stop("Failed to extract the sequence")
-    }else{
+  
+  seq <- readLines(paste("http://www.ncbi.nlm.nih.gov/entrez/batchseq.cgi?",
+                         "cmd=&txt=on&save=&cfm=&list_uids=", gi, "&",
+                         "db=nucleotide&extrafeat=16&term=&view=fasta&",
+                         "dispmax=20&SendTo=t&__from=&__to=&__strand=", sep = ""))
+  
+  if(length(seq) == 0){
+    stop("Failed to extract the sequence")
+  }else{
         return(paste(seq[2:length(seq)], sep = "", collapse = ""))
-    }
+      }
 }
+
+revBase <- function(x){
+  tot <- which(x == "A")
+  tog <- which(x == "C")
+  toa <- which(x == "T")
+  toc <- which(x == "G")
+  x[tot] <- "T"
+  x[toa] <- "A"
+  x[toc] <- "C"
+  x[tog] <- "G"
+  x
+}
+
+revString <- function(x)
+  sapply(lapply(lapply(strsplit(x, NULL), rev), revBase), paste, collapse="")
