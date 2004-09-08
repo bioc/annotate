cf7b42dabb299183cb3991eb52b7685fdc154ed4
Jeff Gentry <jgentry@jimmy.harvard.edu>
2004-09-08 21:02:25 +0000

    updated serializeEnv to handle the new XML format, still need to update man
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/annotate@9892 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/serializeEnv.R
+++ R/serializeEnv.R
@@ -1,10 +1,23 @@
-serializeEnv <- function(env, fname, asList=TRUE) {
+serializeEnv <- function(env, fname) {
  if (!is.character(fname))
      stop("conn should be a character name of file for storage")
 
- if (asList)
-     env <- as.list(env)
+ envList <- as.list(env)
+ keys <- names(envList)
 
- .saveRDS(env, fname)
+ out <- "<?xml version=\"1.0\"?>\n"
+ out <- paste(out,
+              "<values xmlns:bt=\"http://www.bioconductor.org/RGDBM\">")
+ for (i in seq(along=envList)) {
+     out <- paste(out, "\n\t<entry>\n\t\t<key>\n\t\t\t", keys[i],
+                  "\n\t\t</key>\n\t\t<value>\n\t\t\t",
+                  serialize(envList[[i]], NULL, ascii=TRUE),
+                  "\n\t\t</value>\n\t</entry>", sep="")
+
+ }
+ out <- paste(out, "\n</values>", sep="")
+
+ outFile <- gzfile(fname)
+ cat(out, file=outFile)
 }
 
