8efddb8d4bd5e87ff9af11fcd925979069a7b742
Robert Gentleman <rgentlem@fhcrc.org>
2007-09-20 16:32:01 +0000

    fixed a typo in a warning
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/annotate@27349 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/readGEOAnn.R
+++ R/readGEOAnn.R
@@ -72,7 +72,7 @@ readUrl <- function(url){
     con <- try(url(url, open = "r"))
     options(show.error.messages = TRUE)
     if(inherits(con, "try-error")){
-        stop(paste("Can't connent to url", url))
+        stop(paste("Can't connect to url", url))
     }
     temp <- readLines(con)
     close(con)
