6a47b6b24502913428da80b2c60908f0743dad0e
Dan Tenenbaum <dtenenba@fhcrc.org>
2011-12-20 19:51:40 +0000

    For blastSequences(), use REST-ish approach documented here: http://www.ncbi.nlm.nih.gov/blast/Doc/node2.html
    Instead of checking every twenty seconds (three times, then giving up),
    retrieve the estimated job completion time from the response to the original
    query, then wait that long, and look to see if the query is complete.
    
    If not, wait 10 seconds and try again (do this three times, then give up).
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_9/madman/Rpacks/annotate@61492 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/blastSequences.R
+++ R/blastSequences.R
@@ -1,69 +1,85 @@
 ## Try three times and give error if all attempts fail.
 .tryParseResult <- function(url){
-  for(i in 1:4){
-     tryResult <- try(xmlTreeParse(url, useInternalNodes=TRUE), silent=TRUE)
-     if(is(tryResult,"try-error") && i < 4){
-         Sys.sleep(20)
-     }else if(is(tryResult,"try-error") && i >= 4){
-         msg = paste("After 3 attempts, annotate is still not getting", 
-                     "results from the web service. Please try again later.", 
-                      sep=" ")
-         stop(paste(strwrap(msg,exdent=2), collapse="\n"))  
-     }else{
-	result <- xmlTreeParse(url, useInternalNodes=TRUE)
-	return(result)
-     }
+  tf <- tempfile()
+  
+  for(i in 1:4)
+  {
+      download.file(url, tf)
+      f = file(tf)
+      lines <- readLines(tf)
+      close(f)
+      statusLine <- grep("Status=", lines, value=TRUE)
+      if (length(statusLine) == 0) ## is this XML?
+          return(xmlTreeParse(tf, useInternalNodes=TRUE))
+      if (i < 4)
+        Sys.sleep(10)
   }
+  msg = paste("After 3 attempts, annotate is still not getting", 
+              "results from the web service. Please try again later.", 
+               sep=" ")
+  stop(paste(strwrap(msg,exdent=2), collapse="\n"))  
 }
 
 
+## Using the REST-ish API described at
+## http://www.ncbi.nlm.nih.gov/blast/Doc/node2.html
 blastSequences <- function(x,database="nr",
                            hitListSize="10",
                            filter="L",
                            expect="10",
                            program="blastn"){
+   ## TODO: lots of argument checking and testing.  Also,
+   ## depending on which program string is used we need to make the correct
+   ## kind of object at the end (so blastn means DNAMultipleAlignment, and
+   ## blastp means AAMultipleAlignment etc.
 
-  ## TODO: lots of argument checking and testing.  Also,
-  ## depending on which program string is used we need to make the correct
-  ## kind of object at the end (so blastn means DNAMultipleAlignment, and
-  ## blastp means AAMultipleAlignment etc.
-  
-  ## So:
-  ## 1) get online values these parameters can be
-  ## 2) document those
-  ## 3) restrict their vals in the code here.
-  ## 4) for program, use this to determine what object is returned.
-  
-  require(XML)
-  ## assemble the query
-  baseUrl <- "http://www.ncbi.nlm.nih.gov/blast/Blast.cgi"
-  query <- paste("QUERY=",as.character(x),"&DATABASE=",database,
-                 "&HITLIST_SIZE=",hitListSize,"&FILTER=",filter,
-                 "&EXPECT=",expect,"&PROGRAM=",program, sep="")
-  url0 <- sprintf("%s?%s&CMD=Put", baseUrl, query)
+   ## So:
+   ## 1) get online values these parameters can be
+   ## 2) document those
+   ## 3) restrict their vals in the code here.
+   ## 4) for program, use this to determine what object is returned.
+   
+   ## assemble the query
+   baseUrl <- "http://www.ncbi.nlm.nih.gov/blast/Blast.cgi"
+   query <- paste("QUERY=",as.character(x),"&DATABASE=",database,
+                  "&HITLIST_SIZE=",hitListSize,"&FILTER=",filter,
+                  "&EXPECT=",expect,"&PROGRAM=",program, sep="")
+   url0 <- sprintf("%s?%s&CMD=Put", baseUrl, query)
+   results <- tempfile()
+   ## wait 5 seconds before request, to be polite, as requested
+   ## by the documentation (if we are going to make several requests).
+   Sys.sleep(5)
+   resCode <- download.file(url0, results)
+   f = file(results)
+   lines <- readLines(f)
+   close(f)
+   ## Look for job ID
+   ridLine <- grep("RID = ([.]*)", lines, value=TRUE)
+   m <- regexec("RID = ([^\n]*)", ridLine)
+   rm <- regmatches(ridLine, m)
+   rid <- rm[[1]][2]
+   ## Look for RTOE (estimated time to completion)
+   rtoeLine <- grep("RTOE = ",  lines, value=TRUE)
+   m <- regexec("RTOE = ([^\n]*)", rtoeLine)
+   rm <- regmatches(rtoeLine, m)
+   rtoe <- as.integer(rm[[1]][2])
+   url1 <- sprintf("%s?RID=%s&FORMAT_TYPE=XML&CMD=Get", baseUrl, rid)
+   ## wait RTOE seconds
+   Sys.sleep(rtoe)
+   require(XML)
+   result <- .tryParseResult(url1)
+   qseq <- xpathApply(result, "//Hsp_qseq", xmlValue)
+   hseq <- xpathApply(result, "//Hsp_hseq", xmlValue)
 
-  ## Then submit the query and extract the return result id (RID) using XML
-  ## and XPATH queries (see http://www.w3.org/TR/xpath/, especially section 2.5)
-  post <- htmlTreeParse(url0, useInternalNodes=TRUE)
-  rid <- xpathApply(post, '//input[@name="RID"][@id="rid"]',
-                    xmlAttrs)[[1]][["value"]]
-
-  ## and then finally retrieve and parse the result
-  url1 <- sprintf("%s?RID=%s&FORMAT_TYPE=XML&CMD=Get", baseUrl, rid)
-  ## Service needs TIME to generate this file.  So try a few times as needed.
-  Sys.sleep(20) ## almost always seems to need this much sleep.  
-  result <- .tryParseResult(url1)
-  qseq <- xpathApply(result, "//Hsp_qseq", xmlValue)
-  hseq <- xpathApply(result, "//Hsp_hseq", xmlValue)
-
-  ## Instead lets put it into a DNAStringSet and make a MultipleSeqAlignment
-  ## out of it.
-  require(Biostrings)
-  res <- list()
-  for(i in seq_len(length(qseq))){
-    res[i] <- DNAMultipleAlignment( c(hseq[[i]],qseq[[i]]),
-                                    rowmask=as(IRanges(), "NormalIRanges"),
-                                    colmask=as(IRanges(), "NormalIRanges"))
-  }
-  res 
+   ## Instead lets put it into a DNAStringSet and make a MultipleSeqAlignment
+   ## out of it.
+   require(Biostrings)
+   res <- list()
+   for(i in seq_len(length(qseq))){
+     res[i] <- DNAMultipleAlignment( c(hseq[[i]],qseq[[i]]),
+                                     rowmask=as(IRanges(), "NormalIRanges"),
+                                     colmask=as(IRanges(), "NormalIRanges"))
+   }
+   res 
 }
+
